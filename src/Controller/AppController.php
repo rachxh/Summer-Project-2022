cdf96bd8a374d5e43af7978d7563f20aac1d7b4b
Your Name <you@example.com>
2022-05-26 19:02:46 +0300

    created api endpoint, displayed basic table

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,14 +8,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Plants;
 
+#[Route('/api', name: 'api_main')]
 class AppController extends AbstractController
 {
-#[Route('/', name: 'app_homepage')]
-public function index(EntityManagerInterface $em): Response
-{   
-    $plants = $em->getRepository(Plants::class)->findBy([], ['id' => 'ASC']);
-    return $this->render('spa/index.html.twig', [
-        'plants' => $plants
-    ]);
-}
-}
+    #[Route('/plants', name: 'plant_list')]
+    public function index(EntityManagerInterface $em): Response
+    {   
+        $plants = $em->getRepository(Plants::class)->findBy([], ['id' => 'ASC']);
+        return $this->render('spa/index.html.twig', [
+            'plants' => $plants
+        ]);
+    }
+    }
