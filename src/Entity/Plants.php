a88631dd0357e59c6d68e1addced7061e6d13d4e
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2022-05-26 21:58:13 +0300

    added the rest of the database, added all images

--- src/Entity/Plants.php
+++ src/Entity/Plants.php
@@ -19,6 +19,21 @@ class Plants
     #[ORM\Column(type: 'text')]
     private $img;
 
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $name_2;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $Water;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $conditions;
+
+    #[ORM\Column(type: 'integer')]
+    private $difficulty;
+
+    #[ORM\Column(type: 'boolean')]
+    private $pets;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -44,6 +59,66 @@ class Plants
     public function setImg(string $img): self
     {
         $this->img = $img;
+
+        return $this;
+    }
+
+    public function getName2(): ?string
+    {
+        return $this->name_2;
+    }
+
+    public function setName2(?string $name_2): self
+    {
+        $this->name_2 = $name_2;
+
+        return $this;
+    }
+
+    public function getWater(): ?string
+    {
+        return $this->Water;
+    }
+
+    public function setWater(?string $Water): self
+    {
+        $this->Water = $Water;
+
+        return $this;
+    }
+
+    public function getConditions(): ?string
+    {
+        return $this->conditions;
+    }
+
+    public function setConditions(?string $conditions): self
+    {
+        $this->conditions = $conditions;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(int $difficulty): self
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+
+    public function isPets(): ?bool
+    {
+        return $this->pets;
+    }
+
+    public function setPets(bool $pets): self
+    {
+        $this->pets = $pets;
 
         return $this;
     }
