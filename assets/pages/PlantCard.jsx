50ecc584971e9bc569f02d0fe3138b24eb7d91bd
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-05-30 23:41:30 +0300

    got plant list displayed

--- assets/pages/PlantCard.jsx
+++ assets/pages/PlantCard.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const PlantCard = (props) => {
   return (
     <div className="card">
-      <img src={props.img} alt={props.name} />
-      <h3>{props.name}</h3>
-      <Link to={`${props.name}`} style={{ color: "purple" }}>
+      <img className="plant-img" src={props.img} alt={props.name} />
+      <h4 className="card-name">{props.name}</h4>
+      <Link to={`plant/${props.name}`} style={{ color: "green" }}>
         See more
       </Link>
     </div>
