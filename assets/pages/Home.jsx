c35d2662e675f440181949ed56c3489463a0c683
Xiaohong Chen <s2200116@BC-C02GR0BGQ6LR-MacBook-Air.local>
2022-06-02 00:29:53 +0200

    Implement filter

--- assets/pages/Home.jsx
+++ assets/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <h1 className="banner-title">Take care of your plants </h1>
     <p className="lead my-4 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint quaerat inventore sit ipsum illo velit ex, quos quisquam nam veniam?</p>
 
-   <button className="btn btn-success btn-lg btn-banner " data-bs-toggle="modal" data-bs-target="#login">Login</button>
+   <button className= " btn-banner " data-bs-toggle="modal" data-bs-target="#login">Login</button>
 
     </div>
   <img className="img-fluid w-50 d-none d-sm-block"  src="https://cdn.pixabay.com/photo/2019/04/10/12/32/house-plants-4116992__480.png" alt="" />
