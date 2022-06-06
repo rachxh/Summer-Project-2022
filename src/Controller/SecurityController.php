ed233ce301b59e2975d5b939623f87749ef873dd
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2022-06-06 13:41:31 +0300

    edited security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,39 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use ApiPlatform\Core\Api\IriConverterInterface;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login', methods:['POST'])]
-    public function login(IriConverterInterface $iriConverter): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            return $this->json([
-                'error' => 'Invalid login request: check that the Content-Type header is "application/json".'
-            ], 400);
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
         }
-        
-        return $this->json([
-
-            'user' => $this->getUser() ? $this->getUser()->getId() : null]
-        );
-
-        return new Response(null, 204, [
-            'Location' => $iriConverter->getIriFromItem($this->getUser())
-        ]);
-        
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('app_home');
-        // }
 
         // get the login error if there is one
-        // $error = $authenticationUtils->getLastAuthenticationError();
+        $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
-        // $lastUsername = $authenticationUtils->getLastUsername();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        // return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
