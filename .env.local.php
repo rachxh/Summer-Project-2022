f06ae226e085af09c5678d49af805c603b105f18
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2022-06-08 23:27:14 +0300

    env edit

--- .env.local.php
+++ .env.local.php
@@ -6,4 +6,6 @@ return array (
   'APP_ENV' => 'prod',
   'APP_SECRET' => 'a8255cedf849cb84aca9642131f20a68',
   'DATABASE_URL' => 'mysql://root:lionPass@db:3306/PlantDB?serverVersion=5.7',
+  $db = parse_url(getenv("DATABASE_URL")),
+  $db["path"] = ltrim($db["path"], "/")
 );
