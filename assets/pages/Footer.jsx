0b139a24bb7f4d7da0a3a16e117a37a1fdee080c
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2022-06-09 20:43:50 +0300

    edited about text, small additions

--- assets/pages/Footer.jsx
+++ assets/pages/Footer.jsx
@@ -4,12 +4,11 @@ const Footer = () => {
   return (
     <footer className="p-2 bg-dark text-white text-center position-relative">
       <div className="container">
-      <p>copyright&copy; Team Scrums Up 2022</p>
-      <a href="#" className="position-absolute bottom-0 end-0 p-5"> 
-      <i className="bi bi-arrow-up-circle h1"></i>
-      </a>
+        <p>Copyright &copy; Team Scrums Up 2022</p>
+        {/* <a href="#" className="position-absolute bottom-0 end-0 p-5">
+          <i className="bi bi-arrow-up-circle h1"></i>
+        </a> */}
       </div>
-      
     </footer>
   );
 };
