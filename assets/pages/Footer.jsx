4fa2be808eb5e2bd0fd414909ddb0462b40c91d7
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2022-06-09 21:32:35 +0300

    edited footer

--- assets/pages/Footer.jsx
+++ assets/pages/Footer.jsx
@@ -5,9 +5,13 @@ const Footer = () => {
     <footer className="p-2 bg-dark text-white text-center position-relative">
       <div className="container">
         <p>Copyright &copy; Team Scrums Up 2022</p>
-        {/* <a href="#" className="position-absolute bottom-0 end-0 p-5">
-          <i className="bi bi-arrow-up-circle h1"></i>
-        </a> */}
+        <p>
+          Images courtesy
+          <a href="https://www.ikea.com/" className="link">
+            IKEA
+          </a>
+        </p>
+        <p>Used for educational purpose</p>
       </div>
     </footer>
   );
